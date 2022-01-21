7f63351da4b99a155fe769259535b2ef1986c1ac
iamlabib <info.labib@gmail.com>
2022-01-21 21:35:19 +0600

    placeholder image updated

--- src/PhotoEngineController.php
+++ src/PhotoEngineController.php
@@ -11,18 +11,15 @@ class PhotoEngineController extends Controller
         echo "Alhamdulillah for everything";
     }
 
-    public function placeholder($width, $height, $text){
+    public function placeholder($width, $height, $text = null){
         $image = imagecreate($width, $height);
-  
-        // Set the background color of image
-        $background_color = imagecolorallocate($image, 0, 153, 0);
-        
-        // Set the text color of image
+        imagecolorallocate($image, 0, 153, 0);    
         $text_color = imagecolorallocate($image, 255, 255, 255);
         
+        $text = $width . " X " . $height;
         // Function to create image which contains string.
-        imagestring($image, 5, ($width/90), 100,  $text, $text_color);
-        imagestring($image, 3, 160, 120,  "Powered by - PhotoEngine", $text_color);
+        imagestring($image, 5, ($width/2) - 40, ($height/2)-30,  $text, $text_color);
+        imagestring($image, 3, ($width/2) - 80, ($height/2),  "Powered by - PhotoEngine", $text_color);
         
         Header('Content-Type: image/png');
         Imagejpeg($image);
