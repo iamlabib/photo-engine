27773f23b664daf28aef808ed001b461cff056f9
iamlabib <info.labib@gmail.com>
2022-01-21 21:18:59 +0600

    placeholder image updated

--- src/routes.php
+++ src/routes.php
@@ -3,3 +3,4 @@
 use Labib\PhotoEngine\PhotoEngineController;
 
 Route::get('photo-engine', [PhotoEngineController::class, 'index']);
+Route::get('photo-engine/placeholder/{width}/{height}/{text}', [PhotoEngineController::class, 'placeholder']);
